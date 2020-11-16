3e9c5bdc430a5e86574bdf525b9fc2d12792eed3
Andrea Di Cataldi <andrea.dicataldi@gmail.com>
2020-11-16 17:00:27 +0100

    JS
    Added javascript, ticket generation working

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+var btnGenera = document.getElementById("btn-genera");
+var btnAnnulla = document.getElementById("btn-annulla");
+
+btnGenera.addEventListener("click", function () {
+  var offer = "Tariffa Standard";
+  // Selezionare valore input
+  var fullNameInput = document.getElementById("full-name");
+  var kmInput = document.getElementById("km");
+  var fasciaInput = document.getElementById("fascia");
+  // Seleziona i valori
+  var fullName = fullNameInput.value;
+  var km = kmInput.value;
+  var fascia = fasciaInput.value;
+
+  //Calcolo biglietto treno
+  var costPerKm = 0.21;
+  var ticketPrice = costPerKm * km;
+
+  if (fascia == "minorenne") {
+    offer = "Sconto Minorenne";
+    ticketPrice = (ticketPrice * 0.80);
+  } else if (fascia == "over65") {
+    offer = "Sconto Silver";
+    ticketPrice = (ticketPrice * 0.60);
+  }
+
+  document.getElementById("nameDetail").innerHTML = fullName;
+  document.getElementById("scontoDetail").innerHTML = offer;
+  document.getElementById("carrozzaDetail").innerHTML =
+    Math.floor(Math.random() * 9) + 1;
+  document.getElementById("codiceDetail").innerHTML =
+    Math.floor(Math.random() * 9999) + 90000;
+  document.getElementById("costoDetail").innerHTML = ticketPrice.toFixed(2) + "€";
+});
+
+btnAnnulla.addEventListener("click", function () {
+  //Annulla operazione
+  document.getElementById("full-name").innerHTML = ("");
+
+});
